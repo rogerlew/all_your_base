df8f9903c734bcbfc32a7b396ae07cc9db43d32d
Roger Lew <rogerlew@gmail.com>
2024-02-09 16:39:53 -0800

    all_your_base.hydro.determine_water_year

--- hydro/hydro.py
+++ hydro/hydro.py
@@ -3,9 +3,10 @@ from datetime import datetime, timedelta
 
 def determine_wateryear(y, j=None, mo=None):
     if j is not None:
-        mo = int((datetime(int(y), 1, 1) + timedelta(int(j))).month)
+        mo = int((datetime(int(y), 1, 1) + timedelta(int(j) - 1)).month)
 
     if int(mo) > 9:
         return int(y) + 1
 
     return int(y)
+
